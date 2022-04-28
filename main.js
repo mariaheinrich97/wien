d013ed27534e66b05def71d6ef77d47ddc4cbb29
Maria <maria.heinrich@student.uibk.ac.at>
2022-04-28 17:31:40 +0200

    Karte mit Startlayer und Stephansdom als Zentrum

--- main.js
+++ main.js
@@ -2,8 +2,21 @@
 
 
 let stephansdom = {
-lat: 48.208493,
-lng: 16.373118,
-title: "Stephansdom",
+    lat: 48.208493,
+    lng: 16.373118,
+    title: "Stephansdom",
 };
 
+let startLayer = L.tileLayer.provider("BasemapAT.grau")
+
+let map = L.map("map", {
+    center: [stephansdom.lat, stephansdom.lng],
+    zoom: 12,
+    layers: [
+        startLayer
+    ]
+})
+
+let layerControl = L.control.layers ({
+"BasemapAT Grau": startLayer,
+}).addTo(map);
