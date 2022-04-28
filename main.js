27236a451f352d8da4f21bc9643e6d8c2a90d01e
Maria <maria.heinrich@student.uibk.ac.at>
2022-04-28 18:01:30 +0200

    Imperial false als Einstellung im Maßstab

--- main.js
+++ main.js
@@ -45,4 +45,6 @@ let mrk = L.marker([stephansdom.lat, stephansdom.lng].addTo(sightLayer));
 sightLayer.addTo(map);
 
 // Maßstab hinzufügen
-L.control.scale().addTo(map);
+L.control.scale({
+    imperial: false,
+}).addTo(map);
